08e2f6a8c07324bce75a1f620f4667d4e9a688db
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-05-15 22:32:48 -0300

    updates domain stock.py

--- domain/stock.py
+++ domain/stock.py
@@ -3,3 +3,10 @@ class Stock:
         self.name = name
         self.code = code
         self.current_price = current_price
+
+    def format_as_dict(self):
+        return {
+            'name': self.name,
+            'code': self.code,
+            'current_price': self.current_price
+        }
