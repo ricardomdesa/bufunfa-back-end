85de694767777d193145f90ce13bd3448a4344fd
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-05-17 08:43:55 -0300

    fixes upload file spooled temp file

--- use_cases/load_stocks.py
+++ use_cases/load_stocks.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import logging
+from tempfile import SpooledTemporaryFile
 
 LOGGER = logging.getLogger(__name__)
+
+
+def _seekable(self):
+    return self._file.seekable()
+
+
+SpooledTemporaryFile.seekable = _seekable
 
 
 class LoadStocks:
@@ -12,11 +20,11 @@ class LoadStocks:
 
     def run(self, stock_file):
         try:
-            LOGGER.info("teste log", stock_file)
             df = pd.read_excel(stock_file, sheet_name='Stocks', dtype=str)
             df[['stock_current_price']] = df[['stock_current_price']].astype(float)
             docs = df.to_dict('records')
             self.stock_repo.add_many(docs)
             return self.stock_presenter.respond(docs)
-        except Exception:
+        except Exception as e:
+            raise Exception(e)
             return self.stock_presenter.respond_with_error()
