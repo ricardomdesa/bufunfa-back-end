cd3c39c45159090da08baa23175ddc4fd8375bb8
Ricardo Sa <ricardo.sa@shopee.com>
2021-05-18 22:43:31 -0300

    updates test for fetch current prices use case

--- tests/use_cases/test_fetch_current_stock_price.py
+++ tests/use_cases/test_fetch_current_stock_price.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 from repositories.stock_repository import StockRepository
+from presenters.fetch_stock_prices_presenter import FetchStockPricesPresenter
 from use_cases.fetch_current_stock_prices import FetchCurrentStockPrices
+from fastapi import Response, status
 
 
 class TestFetchCurrentStockPrices(TestCase):
     def test_run(self):
         repo = StockRepository('ricardo')
-        fetch = FetchCurrentStockPrices(repo)
-        fetch.run()
-        self.assertEqual(True, False)
+        presenter = FetchStockPricesPresenter()
+        use_case = FetchCurrentStockPrices(repo, presenter)
+        response = use_case.run()
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
