73c63ee64c533d9e6145770f32f11688c1c99a9a
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-05-22 18:06:21 -0300

    adds get_investment presenter

---
+++ presenters/get_investment_presenter.py
@@ -0,0 +1,19 @@
+import json
+from fastapi import Response, status
+
+
+class LoadInvestmentPresenter:
+
+    @staticmethod
+    def respond(docs):
+        response = Response()
+        response.status_code = status.HTTP_200_OK
+        response.body = json.dumps(dict(message=f"{len(docs)} transacoes atualizadas com sucesso")).encode("utf-8")
+        return response
+
+    @staticmethod
+    def respond_with_error(message: str = "Erro ao atualizar transacoes"):
+        response = Response()
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        response.body = json.dumps(dict(message=message)).encode("utf-8")
+        return response
