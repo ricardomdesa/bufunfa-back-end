287729857cd5d9cf2904361cd8e2ef2a512b466f
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-06-20 12:59:10 -0300

    updates use case dashboard

--- src/use_cases/get_dashboard_info.py
+++ src/use_cases/get_dashboard_info.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import pdb
 from statistics import mean
 
 from singleton import Singleton
@@ -19,16 +20,17 @@ class GetDashboardInfo(metaclass=Singleton):
             assets = self.__calculate_assets(investments)
             income = self.__calculate_income(investments)
             dashboard = DashboardInfo(assets, income)
-            return self.__presenter.respond(dashboard.format_as_dict())
-        except Exception as error:
-            return self.__presenter.respond_with_error(error)
+            return self.__presenter.respond([dashboard.format_as_dict()])
+        except Exception as e:
+            return self.__presenter.respond_with_error('Error getting dashboard info')
 
     @staticmethod
     def __calculate_assets(investments):
-        list_values = list(map(lambda x: x.format_as_dict()['valor_investido'], investments))
-        return reduce((lambda x, y: x + y), list_values)
+        list_values = list(map(lambda x: x.format_as_dict()['valor_investido_atual'] if x.format_as_dict()['valor_investido_atual'] else 0, investments))
+        print(list_values)
+        return reduce((lambda x, y: x + y if x else 0), list_values)
 
     @staticmethod
     def __calculate_income(investments):
-        list_values = list(map(lambda x: x.format_as_dict()['valor_investido'], investments))
-        return mean(list_values)
+        list_values = list(map(lambda x: x.format_as_dict()['rendimento'] if x.format_as_dict()['rendimento'] else 0, investments))
+        return round(mean(list_values), 2)
