76b6ebae97908c2493d2adb5f3fd3e6f5ece3332
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-06-20 12:59:56 -0300

    changes inv mock

--- src/tests/use_cases/mocks/investment_mock.py
+++ src/tests/use_cases/mocks/investment_mock.py
@@ -19,7 +19,7 @@ INVESTMENT_MOCK_TWO = [{
     'tipo': 'acao',
     'valor_investido': 2000.0,
     'valor_investido_atual': 2400,
-    'rendimento': 0.20
+    'rendimento': 0.2
 },
     {
         'username': 'teste1',
@@ -31,5 +31,29 @@ INVESTMENT_MOCK_TWO = [{
         'valor_investido': 3000.0,
         'valor_investido_atual': 3300,
         'rendimento': 0.10
+    }
+]
+INVESTMENT_MOCK_TWO_CALC_EMPTY = [
+    {
+    'username': 'teste1',
+    'corretora': 'xp',
+    'codigo': 'TEST3.SA',
+    'valor_medio': 20.0,
+    'quantidade': 100,
+    'tipo': 'acao',
+    'valor_investido': 2000.0,
+    'valor_investido_atual': None,
+    'rendimento': None
+    },
+    {
+        'username': 'teste1',
+        'corretora': 'xp',
+        'codigo': 'TEST4.SA',
+        'valor_medio': 30.0,
+        'quantidade': 100,
+        'tipo': 'acao',
+        'valor_investido': 3000.0,
+        'valor_investido_atual': None,
+        'rendimento': None
     }
 ]
