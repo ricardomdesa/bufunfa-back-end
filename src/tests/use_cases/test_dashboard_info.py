e9ade01f975ee43e8c0e64c29a5478757dabb9c3
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-07-24 15:52:51 -0300

    updates dashboard info TC with unittest mock

--- src/tests/use_cases/test_dashboard_info.py
+++ src/tests/use_cases/test_dashboard_info.py
@@ -1,27 +1,23 @@
+from unittest.mock import Mock
+
 from tests.use_cases.mocks.investment_mock import INVESTMENT_MOCK_TWO, INVESTMENT_MOCK_TWO_CALC_EMPTY
 from tests.use_cases.mocks.mock_investment_repo import MockInvestmentRepo
 from use_cases.get_dashboard_info import GetDashboardInfo
-
-
-class Mock_Presenter:
-    def respond(self, _):
-        return _
-
-    def respond_with_error(self, error):
-        return error
 
 
 def test_dashboard_run():
     invest_repo = MockInvestmentRepo()
     invest_repo.set_username("test_dashboard")
     invest_repo.add_many(INVESTMENT_MOCK_TWO)
-    response = GetDashboardInfo(invest_repo, Mock_Presenter()).run()
-    assert response == [{"assets": 5700, "income_perc": 0.12, "income_value": 700.0}]
+    presenter = Mock()
+    GetDashboardInfo(invest_repo, presenter).run()
+    presenter.respond.assert_called_with([{"assets": 5700, "income_perc": 0.12, "income_value": 700.0}])
 
 
 def test_dashboard_run_without_current_price():
     invest_repo = MockInvestmentRepo()
     invest_repo.set_username("test_dashboard")
+    presenter = Mock()
     invest_repo.add_many(INVESTMENT_MOCK_TWO_CALC_EMPTY)
-    response = GetDashboardInfo(invest_repo, Mock_Presenter()).run()
-    assert response == [{"assets": 0, "income_perc": 0, "income_value": 5000}]
+    GetDashboardInfo(invest_repo, presenter).run()
+    presenter.respond.assert_called_with([{"assets": 0, "income_perc": 0, "income_value": 5000}])
