ffccb2d8b756a20d0c6343d6d95a128a77a5b367
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-08-22 16:59:12 -0300

    removes unused mocks and tests

--- src/tests/use_cases/mocks/mock_investment_presenter.py
+++
@@ -1,8 +0,0 @@
-class MockInvestmentPresenter:
-    @staticmethod
-    def respond(docs):
-        return docs
-
-    @staticmethod
-    def respond_with_error():
-        return b'{"message": "Erro ao buscar lista de investmentos"}'
