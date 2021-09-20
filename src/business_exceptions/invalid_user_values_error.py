d6db7dd6a7a5686f52376bdf94d41b29ed05852f
Ricardo Sa <ricardo.sa@shopee.com>
2021-09-19 21:48:00 -0300

    refactors exceptions

--- src/business_exceptions/invalid_user_values_error.py
+++ src/business_exceptions/invalid_user_values_error.py
@@ -1,3 +1,10 @@
-class InvalidUserValuesError(Exception):
-    def __init__(self, message="Valores de usuario invalidos."):
-        self.message = message
+from .bf_base_exception import BfBaseError
+
+
+class InvalidUserValuesError(BfBaseError):
+    message = "Valores de usuario invalidos."
+
+    def __init__(self, message: str = None, *args) -> None:
+        if message is not None:
+            self.message = message
+        super().__init__(self.message, *args)
--- src/business_exceptions/use_case_exceptions.py
+++ src/business_exceptions/use_case_exceptions.py
@@ -2,6 +2,9 @@ from .bf_base_exception import BfBaseError
 
 
 class EmptyDatabaseError(BfBaseError):
-    def __init__(self, message="Banco de dados vazio"):
-        self.message = message
-        super().__init__(self.message)
+    message = "Banco de dados vazio."
+
+    def __init__(self, message: str = None, *args) -> None:
+        if message is not None:
+            self.message = message
+        super().__init__(self.message, *args)
